edf0475aa918d8850ac68942b9d4529c3b18bc0b
rishidk22 <rishidk22@gmail.com>
2018-08-04 15:11:27 -0500

    changed CORS

--- src/app.py
+++ src/app.py
@@ -25,6 +25,8 @@ def process_chat(chat_input):
 
     return response
 
+
+
 # #----------API FRAMEWORK/PROCCESSING-------------------
 app = Flask(__name__)
 
@@ -37,7 +39,16 @@ def home():
 def chat():
     return process_chat(request.data)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port=port)
+
+

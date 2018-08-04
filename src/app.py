7c3be46581af420877e0cc13855dab7b37421733
rishidk22 <rishidk22@gmail.com>
2018-08-04 02:50:48 -0500

    made changes

--- src/app.py
+++ src/app.py
@@ -35,4 +35,6 @@ def chat():
     return process_chat(request.data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.debug = True
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
